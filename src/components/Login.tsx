e0466c7808ac249130152ab1158b34c2609b0c11
Luke Tchang <ltchang@stanford.edu>
2020-07-19 01:48:24 -0700

    first commit

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -5,56 +5,56 @@ import Web3 from 'web3';
 // let web3: Web3 | undefined = undefined;
 
 class Login extends Component {
-  // state = { account: "" };
+  state = { account: "" };
 
-  // async componentDidMount() {
-  //   try {
-  //     await (window as any).ethereum.enable();
-  //     web3 = new Web3((window as any).ethereum);
-  //   } catch (error) {
-  //     window.alert('You need to allow MetaMask.');
-  //     return;
-  //   }
+  async componentDidMount() {
+    try {
+      await (window as any).ethereum.enable();
+      web3 = new Web3((window as any).ethereum);
+    } catch (error) {
+      window.alert('You need to allow MetaMask.');
+      return;
+    }
 
-  //   // account = await web3.eth.getCoinbase();
-  //   this.setState({ account: "0xc2eEC124f617aa6B17eFc22D4126CA65ce4A9DEb" });
-  // }
+    // account = await web3.eth.getCoinbase();
+    this.setState({ account: "0xc2eEC124f617aa6B17eFc22D4126CA65ce4A9DEb" });
+  }
 
-  // handleClick = async() => {
-  //   const { account } = this.state;
-  //   fetch(`http://localhost:5000/api/user/${account}`)
-  //     .then(response => response.json())
-  //     .then(this.handleSignMessage)
-  // }
+  handleClick = async() => {
+    const { account } = this.state;
+    fetch(`http://localhost:5000/api/user/${account}`)
+      .then(response => response.json())
+      .then(this.handleSignMessage)
+  }
 
-  // //shouldn't be of type <any>!!
-  // handleSignMessage = async (userInfo: any) => {
-  //   const { _id, nonce } = userInfo;
+  //shouldn't be of type <any>!!
+  handleSignMessage = async (userInfo: any) => {
+    const { _id, nonce } = userInfo;
     
-  //   try {
-  //     const signature = await web3!.eth.personal.sign(
-  //       `I am signing my one-time nonce: ${nonce}`,
-  //       _id,
-  //       ''
-  //     );
+    try {
+      const signature = await web3!.eth.personal.sign(
+        `I am signing my one-time nonce: ${nonce}`,
+        _id,
+        ''
+      );
 
-  //     return { _id, signature };
-  //   } catch(err) {
-  //     throw new Error('Must sign message to log in.');
-  //   }
-  //   // fetch(`http://localhost:5000/api/auth`, {
-  //   //   body: JSON.stringify(userInfo),
-  //   //   headers: { 'Content-Type': 'application/json' },
-  //   //   method: 'POST'
-  //   // }).then(response => response.json());
-  // }
+      return { _id, signature };
+    } catch(err) {
+      throw new Error('Must sign message to log in.');
+    }
+    // fetch(`http://localhost:5000/api/auth`, {
+    //   body: JSON.stringify(userInfo),
+    //   headers: { 'Content-Type': 'application/json' },
+    //   method: 'POST'
+    // }).then(response => response.json());
+  }
 
   render() {
     return (
       <div>
         <h1>Fuzzy Ads</h1>
         <Button
-          // onClick={this.handleClick}
+          onClick={this.handleClick}
         >
           Login With MetaMask
         </Button>
