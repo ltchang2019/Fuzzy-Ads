174f723c7ef59a33f6f7417d4cfd20973c7486e7
Luke Tchang <ltchang@stanford.edu>
2020-09-07 21:14:52 -0700

    Update Ad.sol

--- src/ethereum/contracts/Ad.sol
+++ src/ethereum/contracts/Ad.sol
@@ -1,49 +1,49 @@
-// pragma solidity ^0.6.0;
+pragma solidity ^0.6.0;
 
-// import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// import "@openzeppelin/contracts/utils/Counters.sol";
-// import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-// contract Ad is ERC721, Ownable {
+contract Ad is ERC721, Ownable {
     
-//     using Counters for Counters.Counter;
-//     Counters.Counter private _tokenIds;
+     using Counters for Counters.Counter;
+     Counters.Counter private _tokenIds;
 
-//     struct AdvertisingPeriod {
-//         uint256 startTime;
-//         uint256 endTime;
-//     }
+     struct AdvertisingPeriod {
+         uint256 startTime;
+         uint256 endTime;
+     }
     
-//     mapping (uint256 => AdvertisingPeriod) public advertisingPeriods;
-//     mapping (uint256 => string) public metadata;
+     mapping (uint256 => AdvertisingPeriod) public advertisingPeriods;
+     mapping (uint256 => string) public metadata;
 
-//     event AdSlotCreated(uint256 indexed tokenId, address indexed tokenOwner, uint256 indexed startTime, uint256 endTime);
+     event AdSlotCreated(uint256 indexed tokenId, address indexed tokenOwner, uint256 indexed startTime, uint256 endTime);
     
-//     constructor() public ERC721("FuzzyAds", "FUZZY") {}
+     constructor() public ERC721("FuzzyAds", "FUZZY") {}
     
-//     function createAd(uint64 startTime, uint64 endTime, string memory tokenURI) public onlyOwner {
-//         require(startTime > now, "Token must have start time in the future.");
-//         require(startTime < endTime, "Start time must be lower than End time.");
+     function createAd(uint64 startTime, uint64 endTime, string memory tokenURI) public onlyOwner {
+         require(startTime > now, "Token must have start time in the future.");
+         require(startTime < endTime, "Start time must be lower than End time.");
 
-//         _tokenIds.increment();
+         _tokenIds.increment();
         
-//         uint256 newTokenId = _tokenIds.current();
+         uint256 newTokenId = _tokenIds.current();
 
-//         _mint(msg.sender, newTokenId);
-//         _setTokenURI(newTokenId, tokenURI);
+         _mint(msg.sender, newTokenId);
+         _setTokenURI(newTokenId, tokenURI);
 
-//         advertisingPeriods[newTokenId] = AdvertisingPeriod(startTime, endTime);
+         advertisingPeriods[newTokenId] = AdvertisingPeriod(startTime, endTime);
         
-//         emit AdSlotCreated(newTokenId, msg.sender, startTime, endTime);
-//     }
+         emit AdSlotCreated(newTokenId, msg.sender, startTime, endTime);
+     }
 
-//     function getMetadata(uint tokenId) public view returns(string memory){
-//         return metadata[tokenId];
-//     }
+     function getMetadata(uint tokenId) public view returns(string memory){
+         return metadata[tokenId];
+     }
 
-//     function setMetadata(uint256 tokenId, string memory ipfsHash) public {
-//         if(msg.sender == ownerOf(tokenId)){
-//             metadata[tokenId] = ipfsHash;
-//         }
-//     }
-// }
+     function setMetadata(uint256 tokenId, string memory ipfsHash) public {
+         if(msg.sender == ownerOf(tokenId)){
+             metadata[tokenId] = ipfsHash;
+         }
+     }
+}
